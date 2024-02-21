7ba61b2d3a6e7492cdb2f1940c70938120b0f29b
Stuart Shelley <sshelley@ualberta.ca>
2024-02-21 12:53:50 -0700

    create basic UI activity_create_event.xml

--- app/app/src/main/java/com/example/qrcheckin/CreateEventActivity.java
+++ app/app/src/main/java/com/example/qrcheckin/CreateEventActivity.java
@@ -1,7 +1,38 @@
 package com.example.qrcheckin;
 
+import android.os.Bundle;
+import android.widget.EditText;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-public class CreateEventActivity extends AppCompatActivity {
 
+
+
+
+public class CreateEventActivity extends AppCompatActivity {
+    EditText newEventName;
+    EditText newStartTime;
+    EditText newEndTime;
+    EditText newLocation;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_create_event);
+
+        // Bind UI
+        //TODO posterImage = findViewById(R.id.posterImageView);
+        //TODO - EDIT POSTER button
+
+        newEventName = findViewById(R.id.eventNameEditText);
+        newStartTime = findViewById(R.id.eventStartTimeEditText);
+        newEndTime = findViewById(R.id.eventEndTimeEditText);
+        newLocation = findViewById(R.id.eventLocationEditText);
+
+        // TODO On Confirm button click
+        //     send to new fragment
+        //     generate new QR code
+        //
+
+    }
 }
