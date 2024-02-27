9cd27c829390f865d70937e9b0dbd52500ea4f93
Stuart Shelley <sshelley@ualberta.ca>
2024-02-27 13:02:56 -0700

    create QRGenerator class + qr_generator.xml UI

---
+++ app/app/src/main/java/com/example/qrcheckin/QRGenerator.java
@@ -0,0 +1,16 @@
+package com.example.qrcheckin;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class QRGenerator extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.qr_generator);
+
+
+    }
+
+}
