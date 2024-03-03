53cf9b692e6d331f6fc95411e314a62efac49212
Armaan4270 <adas5@ualberta.ca>
2024-03-02 22:25:53 -0700

    updates

---
+++ app/app/src/main/java/com/example/qrcheckin/ViewEventActivity.java
@@ -0,0 +1,14 @@
+package com.example.qrcheckin;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ViewEventActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_view_event);
+    }
+}
