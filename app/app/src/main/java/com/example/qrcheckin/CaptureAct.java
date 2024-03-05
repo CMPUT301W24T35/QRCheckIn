689e416be69f779594a8496e7d45908bc12d75a6
Vincent Liu <binhua1@ualberta.ca>
2024-03-05 12:55:27 -0700

    add a comment

--- app/app/src/main/java/com/example/qrcheckin/CaptureAct.java
+++ app/app/src/main/java/com/example/qrcheckin/CaptureAct.java
@@ -2,5 +2,9 @@ package com.example.qrcheckin;
 
 import com.journeyapps.barcodescanner.CaptureActivity;
 
+/**
+ * It is for customizing the UI and other functionalities for QR scanner
+ */
+//It is for customizing the UI and adding functionalities
 public class CaptureAct extends CaptureActivity {
 }
