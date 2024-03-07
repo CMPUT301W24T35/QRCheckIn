cdb1d4229914239cb5bd8002c85f05704e852ddd
Ann0503 <97485275+Ann0503@users.noreply.github.com>
2024-03-07 12:03:01 -0700

    Update CaptureAct.java

--- app/app/src/main/java/com/example/qrcheckin/CaptureAct.java
+++ app/app/src/main/java/com/example/qrcheckin/CaptureAct.java
@@ -2,5 +2,8 @@ package com.example.qrcheckin;
 
 import com.journeyapps.barcodescanner.CaptureActivity;
 
+/**
+* This is a class for capturing activity
+*/
 public class CaptureAct extends CaptureActivity {
 }
