03f1b53bfe0a2969d24c038bfd29d2949b01c355
“Humbledeep <humbledeep.singh@gmail.com>
2024-03-07 12:10:41 -0700

    Unit testing updates

--- app/app/src/androidTest/java/com/example/qrcheckin/CreateProfileActivityTest.java
+++ app/app/src/androidTest/java/com/example/qrcheckin/CreateProfileActivityTest.java
@@ -5,11 +5,21 @@ import androidx.test.ext.junit.runners.AndroidJUnit4;
 import androidx.test.filters.LargeTest;
 
 import org.junit.Rule;
+import org.junit.Test;
 import org.junit.runner.RunWith;
 
 @RunWith(AndroidJUnit4.class)
 @LargeTest
-public class MainActivityTest {
+public class CreateProfileActivityTest {
     @Rule
-    public ActivityScenarioRule<MainActivity> scenario = new ActivityScenarioRule<MainActivity>(MainActivity.class);
+    public ActivityScenarioRule<CreateProfileActivity> scenario = new
+            ActivityScenarioRule<CreateProfileActivity>(CreateProfileActivity.class);
+
+    @Test
+    public void test(){
+
+
+    }
+
+
 }
