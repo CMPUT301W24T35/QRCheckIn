53f0fd16c11e78b22e205c352f499f1b459772e6
Ann0503 <97485275+Ann0503@users.noreply.github.com>
2024-03-07 13:12:25 -0700

    Update CreateEventActivity.java

--- app/app/src/main/java/com/example/qrcheckin/CreateEventActivity.java
+++ app/app/src/main/java/com/example/qrcheckin/CreateEventActivity.java
@@ -1,17 +1,11 @@
 package com.example.qrcheckin;
 
-<<<<<<< HEAD
 import android.content.Intent;
 import android.graphics.Bitmap;
 import android.os.Bundle;
 import android.util.Log;
 import android.widget.Button;
 import android.widget.CheckBox;
-=======
-import android.os.Bundle;
-import android.util.Log;
-import android.widget.Button;
->>>>>>> 7044ab24b655fd5348f453537fe799830ec8bda8
 import android.widget.EditText;
 import android.widget.ImageView;
 
@@ -21,7 +15,6 @@ import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.bumptech.glide.Glide;
-<<<<<<< HEAD
 import com.google.firebase.firestore.FirebaseFirestore;
 
 import java.util.HashMap;
@@ -53,26 +46,6 @@ public class CreateEventActivity extends AppCompatActivity {
 
     Bundle bundle;
 
-
-=======
-
-import kotlinx.coroutines.channels.Send;
-
-
-public class CreateEventActivity extends AppCompatActivity {
-    EditText newEventName;
-    EditText newStartTime;
-    EditText newEndTime;
-    EditText newLocation;
-    Button confirmButton;
-    Button editPosterImageButton;
-
-    Button generateQRCodeButton;
-    Button reuseQRCodeButton;
-
-    ImageView posterImage;
-
->>>>>>> 7044ab24b655fd5348f453537fe799830ec8bda8
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -84,7 +57,6 @@ public class CreateEventActivity extends AppCompatActivity {
         //TODO - EDIT POSTER button
 
         newEventName = findViewById(R.id.eventNameEditText);
-<<<<<<< HEAD
         newEventDescription = findViewById(R.id.eventDescriptionEditText);
         newStartTime = findViewById(R.id.eventStartTimeEditText);
         newEndTime = findViewById(R.id.eventEndTimeEditText);
@@ -94,17 +66,8 @@ public class CreateEventActivity extends AppCompatActivity {
         posterImage = findViewById(R.id.posterImageView);
         generatePromoQRCodeCheckbox = findViewById(R.id.checkboxGeneratePromoQRCode);
         db = FirebaseFirestore.getInstance();
-        // TODO Optional Field - limit number of attendees
-=======
-        newStartTime = findViewById(R.id.eventStartTimeEditText);
-        newEndTime = findViewById(R.id.eventEndTimeEditText);
-        newLocation = findViewById(R.id.eventLocationEditText);
         confirmButton = findViewById(R.id.confirmCreateEventButton);
-        editPosterImageButton = findViewById(R.id.editPosterImageButton);
-        posterImage = findViewById(R.id.posterImageView);
-        generateQRCodeButton = findViewById(R.id.generateQRCodeButton);
         reuseQRCodeButton = findViewById(R.id.reuseQRCodeButton);
->>>>>>> 7044ab24b655fd5348f453537fe799830ec8bda8
 
         // Registers a photo picker activity launcher in single-select mode.
         // Source: https://developer.android.com/training/data-storage/shared/photopicker#select-single-item
@@ -132,8 +95,6 @@ public class CreateEventActivity extends AppCompatActivity {
             }
         );
 
-
-<<<<<<< HEAD
     // TODO - Continue button
         //continueButton.setOnClickListener(v -> startNextActivity());
         // TEST
@@ -291,19 +252,5 @@ public class CreateEventActivity extends AppCompatActivity {
                         isDBConnected = false;
                     }
                 });
-=======
-    // TODO - Generate QR Code
-        // Generate new QR Code
-        // Set to display
-
-
-    // TODO - Reuse QR Code
-
-    // TODO - Confirm button
-        // Gather all data entered inc PosterImage, QRCode
-        // Perform data input checks
-        // Write data to db
-
->>>>>>> 7044ab24b655fd5348f453537fe799830ec8bda8
     }
 }
