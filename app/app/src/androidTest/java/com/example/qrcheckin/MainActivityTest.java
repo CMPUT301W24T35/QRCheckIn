e9f93d1e6be716f17e2d5f3ab38be4fff4a201d4
“Humbledeep <humbledeep.singh@gmail.com>
2024-03-07 11:53:20 -0700

    Unit testing

---
+++ app/app/src/androidTest/java/com/example/qrcheckin/MainActivityTest.java
@@ -0,0 +1,15 @@
+package com.example.qrcheckin;
+
+import androidx.test.ext.junit.rules.ActivityScenarioRule;
+import androidx.test.ext.junit.runners.AndroidJUnit4;
+import androidx.test.filters.LargeTest;
+
+import org.junit.Rule;
+import org.junit.runner.RunWith;
+
+@RunWith(AndroidJUnit4.class)
+@LargeTest
+public class MainActivityTest {
+    @Rule
+    public ActivityScenarioRule<MainActivity> scenario = new ActivityScenarioRule<MainActivity>(MainActivity.class);
+}
