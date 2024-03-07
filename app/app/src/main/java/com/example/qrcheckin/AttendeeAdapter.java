df9dfa253536482061380676c69318789399578d
Ann0503 <97485275+Ann0503@users.noreply.github.com>
2024-03-07 12:01:06 -0700

    Update AttendeeAdapter.java

--- app/app/src/main/java/com/example/qrcheckin/AttendeeAdapter.java
+++ app/app/src/main/java/com/example/qrcheckin/AttendeeAdapter.java
@@ -13,6 +13,9 @@ import androidx.annotation.Nullable;
 import java.util.ArrayList;
 import java.util.List;
 
+/**
+* This is a class that responsible for displaying a list of attendees in a ListView or Spinner
+*/
 public class AttendeeAdapter extends ArrayAdapter<Profile> {
     private Context context;
     private ArrayList<Profile> attendees;
