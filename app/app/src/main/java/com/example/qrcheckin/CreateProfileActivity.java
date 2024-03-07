12da9ef6ff31affbf9aa5274ade859f9fa45c605
Ayan <aabbas@ualberta.ca>
2024-03-07 11:26:34 -0700

    fixed merge conficts

--- app/app/src/main/java/com/example/qrcheckin/CreateProfileActivity.java
+++ app/app/src/main/java/com/example/qrcheckin/CreateProfileActivity.java
@@ -1,9 +1,7 @@
 package com.example.qrcheckin;
 
 
-import android.app.Activity;
 import android.content.Context;
-
 import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.Canvas;
